552dc21064b26a67526cfe5fb13574b2f3b89f25
Ricardo Mesquita e Castro <74441094+RicardoMesquitaCastro@users.noreply.github.com>
2022-05-18 22:06:27 -0300

    web-build

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Routes from './pages/routes';
-import { Amplify } from 'aws-amplify'
-import awsconfig from './src/aws-exports'
+//import { Amplify } from 'aws-amplify'
+//import awsconfig from './src/aws-exports'
 
 const Stack = createStackNavigator();
 
-Amplify.configure(awsconfig)
+//Amplify.configure(awsconfig)
 const App = () => {
     return (
         <NavigationContainer>
